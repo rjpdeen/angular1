04fb35accaa8927e3cbf6a363a85096c39e91b67
Ron Deen <rjp.deen@gmail.com>
2018-01-18 08:57:09 +0100

    3

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 (function () {
 'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('LunchCheck', [])
 
-.controller('MyFirstController', function ($scope) {
-	$scope.name = "Ron"
-	$scope.myName = function (){ 
-		return $scope.name + "!!!!";
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
+	$scope.lunch_input = "";
+	$scope.message = "";
+
+	$scope.displayMessage = function () {
+		$scope.message = checkTooMuch($scope.lunch_input);
+	};
+
+	function checkTooMuch (string) {
+		if (string == "") {
+			return "Please enter data first";
+		}
+		else {
+			var count = 0;
+			for (var i=0;i<string.length;i++) {
+				if (string.charAt(i) == ",") {
+					count++;
+				}
+			}
+			
+			if (count < 3) {
+				return "Enjoy!";
+			}
+			else {
+				return "Too much!";
+			}
+		}
 	}
-});
+
+};
 
 })();
